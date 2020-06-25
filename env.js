9a6b9d99becc6e946f3662d2c54592ddae2a9ade
Brijesh <brijesh.a@yudiz.in>
2020-06-25 15:12:47 +0530

    Made cloudinary config global

--- env.js
+++ env.js
@@ -1,18 +1,19 @@
 const cloudinary = require('cloudinary').v2
 const environment = {}
 
-environment.dev = {
-  DB_URL: 'mongodb+srv://brijesh_1116:mLab17@mycluster0-xnamd.mongodb.net/pollManagement?retryWrites=true&w=majority',
-  PORT: '3000',
-  JWT_KEY: 'secretKey',
-  SALT_NUMBER: 10,
-  CLOUDINARY_URL: 'cloudinary://786611446216667:mQu7l7wxU1VGdWxmTpIFXx_wWcg@yudiz-solutions'
-}
 environment.cloud = cloudinary.config({
   cloud_name: 'yudiz-solutions',
   api_key: '786611446216667',
   api_secret: 'mQu7l7wxU1VGdWxmTpIFXx_wWcg'
 })
+
+environment.dev = {
+  DB_URL: 'mongodb+srv://brijesh_1116:mLab17@mycluster0-xnamd.mongodb.net/pollManagement?retryWrites=true&w=majority',
+  PORT: '3000',
+  JWT_KEY: 'secretKey',
+  SALT_NUMBER: 10,
+  CLOUDINARY_URL: `cloudinary://${environment.cloud.api_key}:${environment.cloud.api_secret}@${environment.cloud.cloud_name}`
+}
 
 process.env.JWT_KEY = environment.dev.JWT_KEY
 process.env.DB_URL = environment.dev.DB_URL
